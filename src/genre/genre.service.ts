11daf4cd13e007cd15e69359110ff46edc12680a
bm4656 <newjin46@gmail.com>
2025-06-20 02:46:02 +0900

    fix: 장르 생성 중복 오류 추가

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -26,7 +26,13 @@ export class GenreService {
     return genre;
   }
 
-  create(createGenreDto: CreateGenreDto) {
+  async create(createGenreDto: CreateGenreDto) {
+    const genre = await this.genreRepository.findOne({ where: { name: createGenreDto.name } });
+
+    if (genre) {
+      throw new NotFoundException('이미 존재하는 장르입니다!');
+    }
+
     return this.genreRepository.save(createGenreDto);
   }
 

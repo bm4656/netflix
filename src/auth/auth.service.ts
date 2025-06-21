45c2ee2c6cd8fd42ca5dc75e686a6a232921f9bc
bm4656 <newjin46@gmail.com>
2025-06-21 23:16:04 +0900

    feat: Cursor Based Pagination 구현(기본)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -127,7 +127,7 @@ export class AuthService {
       },
       {
         secret: isRefreshToken ? refreshTokenSecret : accessTokenSecret,
-        expiresIn: isRefreshToken ? '24h' : 300,
+        expiresIn: isRefreshToken ? '24h' : '1h',
       },
     );
   }

ec9e27c0e355fe493ef0773ad036340734e75379
bm4656 <newjin46@gmail.com>
2025-06-23 22:39:39 +0900

    feat: 멀티칼럼 Cursor Based Pagination 3
    common.service.ts
    - cursor 디코드 후 로직 구현

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -17,9 +17,27 @@ export class CommonService {
   }
 
   async applyCursorPaginationParamsToQb<T>(qb: SelectQueryBuilder<T>, dto: CursorPaginationDto) {
-    const { cursor, order, take } = dto;
+    const { cursor, take } = dto;
+    let { order } = dto;
 
     if (cursor) {
+      const decodedCursor = Buffer.from(cursor, 'base64').toString('utf-8');
+      const cursorObj = JSON.parse(decodedCursor);
+
+      const { values } = cursorObj;
+
+      order = cursorObj.order; // 커서에서 받은 order를 사용 -> 혹시나 FE에서 order를 바꿨을 때 BE에서 커서가 잘못된 경우를 방지하기 위함
+
+      // WHERE (column1 > value1)
+      // OR (column1 = value1 AND column2 > value2)
+      // OR (column1 = value1 AND column2 = value2 AND column3 > value3)
+      // ( column1, column2, ... ) > ( :value1, :value2, ... )
+      const columns = Object.keys(values);
+      const comparisonOperator = order.some((o) => o.endsWith('DESC')) ? '<' : '>'; // order가 DESC로 끝나는지 확인 -> 우리는 모든 컬럼의 order가 동일하다고 가정
+      const whereConditions = columns.map((c) => `${qb.alias}.${c}`).join(',');
+      const whereParams = columns.map((c) => `:${c}`).join(',');
+
+      qb.where(`(${whereConditions}) ${comparisonOperator} (${whereParams})`, values);
     }
 
     for (let i = 0; i < order.length; i++) {

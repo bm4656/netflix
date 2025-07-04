e4da62f1fb5279b7285bb942d3e7b5db6d301f0d
bm4656 <newjin46@gmail.com>
2025-07-04 21:45:22 +0900

    feat: Swagger ApiBearerAuth() 설정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,16 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './strategy/local.strategy';
 import { JwtAuthGuard } from './strategy/jwt.strategy';
 import { Public } from './decorator/public.decorator';
-import { ApiBasicAuth } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiBearerAuth } from '@nestjs/swagger';
 import { Authorization } from './decorator/authorization.decorator';
 
 @Controller('auth')
+@ApiBearerAuth()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
+  @ApiBasicAuth()
   @Post('register')
   register(@Authorization() token: string) {
     return this.authService.register(token);
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -24,8 +24,10 @@ import { QueryRunner } from '../common/decorator/query-runner.decorator';
 import { QueryRunner as QR } from 'typeorm';
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { Throttle } from '../common/decorator/throttle.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('movie')
+@ApiBearerAuth()
 @UseInterceptors(ClassSerializerInterceptor)
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}

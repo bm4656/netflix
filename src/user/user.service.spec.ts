a3499c9f88b6274ea82ad583dfaced133777749c
bm4656 <newjin46@gmail.com>
2025-07-16 23:57:59 +0900

    feat: user 유닛테스트 사전 준비 mockRepository 설정
    user.controller.spec.ts
    - 먼저 서비스 작성 후 컨트롤러 작성할 예정이므로 임시 주석 처리

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+
+const mockUserRepository = {
+  findOne: jest.fn(),
+  find: jest.fn(),
+  save: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          // getRepositoryToken(model): TypeORM에서 해당 모델의 레포지토리를 가져오는 함수
+          // Module에서 TypeORM.forFeature([User])와 같은 역할을 함
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository, // 해당 레포지토리로 대체해서 사용하도록 설정
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);

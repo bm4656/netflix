0e81f8e3347f2ce205705198dfa3fd390b7a3e38
bm4656 <newjin46@gmail.com>
2025-07-20 23:59:29 +0900

    test: userService findAll 테스트 코드 작성

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -12,7 +12,7 @@ const mockUserRepository = {
 };
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,10 +27,27 @@ describe('UserService', () => {
       ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return all users', async () => {
+      const users = [
+        {
+          id: 1,
+          email: 'test@test.ai',
+        },
+      ];
+      mockUserRepository.find.mockResolvedValue(users);
+
+      const result = await userService.findAll();
+
+      expect(result).toEqual(users);
+      expect(mockUserRepository.find).toHaveBeenCalled();
+    });
   });
 });

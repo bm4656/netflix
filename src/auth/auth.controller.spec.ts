048c9d98ae361b50f23f5a53289f5fcfd589d665
bm4656 <newjin46@gmail.com>
2025-07-29 20:35:29 +0900

    test: auth controller 테스트 코드 작성

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,19 +1,110 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User } from '../user/entity/user.entity';
+
+const mockAuthService = {
+  register: jest.fn(),
+  login: jest.fn(),
+  tokenBlock: jest.fn(),
+  issueToken: jest.fn(),
+};
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
-    // const module: TestingModule = await Test.createTestingModule({
-    //   controllers: [AuthController],
-    //   providers: [AuthService],
-    // }).compile();
-    //
-    // controller = module.get<AuthController>(AuthController);
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    }).compile();
+
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    // expect(controller).toBeDefined();
-    expect(true).toBe(true);
+    expect(authController).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should register a user', () => {
+      const token = 'Basic dsivjoxicjvsdf';
+      const result = { id: 1, email: 'test@codefactory.ai' };
+
+      jest.spyOn(authService, 'register').mockResolvedValue(result as User);
+
+      expect(authController.register(token)).resolves.toEqual(result);
+      expect(authService.register).toHaveBeenCalledWith(token);
+    });
+  });
+
+  describe('login', () => {
+    it('should loign a user', async () => {
+      const token = 'Basic asdivjoxicjv';
+      const result = {
+        refreshToken: 'mocked.refresh.token',
+        accessToken: 'mocked.access.token',
+      };
+
+      jest.spyOn(authService, 'login').mockResolvedValue(result);
+
+      expect(authController.login(token)).resolves.toEqual(result);
+      expect(authService.login).toHaveBeenCalledWith(token);
+    });
+  });
+
+  describe('blockToken', () => {
+    it('should block a token', async () => {
+      const token = 'some.jwt.token';
+      jest.spyOn(authService, 'tokenBlock').mockResolvedValue(true);
+
+      expect(authController.tokenBlock(token)).resolves.toBe(true);
+      expect(authService.tokenBlock).toHaveBeenCalledWith(token);
+    });
+  });
+
+  describe('rotateAccessToken', () => {
+    it('should rotate access token', async () => {
+      const accessToken = 'mocked.access.token';
+
+      jest.spyOn(authService, 'issueToken').mockResolvedValue(accessToken);
+
+      const result = await authController.rotateAccessToken({ user: 'a' });
+
+      expect(authService.issueToken).toHaveBeenCalledWith('a', false);
+      expect(result).toEqual({ accessToken });
+    });
+  });
+
+  describe('loginUserPassport', () => {
+    it('should login user using passport strategy', async () => {
+      const user = { id: 1, role: 'user' };
+      const req = { user };
+      const accessToken = 'mocked.access.token';
+      const refreshToken = 'mocked.refresh.token';
+
+      jest
+        .spyOn(authService, 'issueToken')
+        .mockResolvedValueOnce(refreshToken)
+        .mockResolvedValueOnce(accessToken);
+
+      const result = await authController.loginUserPassport(req);
+
+      expect(authService.issueToken).toHaveBeenCalledTimes(2);
+      expect(authService.issueToken).toHaveBeenNthCalledWith(1, user, true);
+      expect(authService.issueToken).toHaveBeenNthCalledWith(2, user, false);
+      expect(result).toEqual({ refreshToken, accessToken });
+    });
   });
 });
